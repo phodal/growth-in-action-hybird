6d27b9fff3cd28c2543c60e94b536709b142a201
Fengda HUANG <h@phodal.com>
2016-05-12 01:30:59 +0800

    Add authhttp

--- app/app.ts
+++ app/app.ts
@@ -4,6 +4,9 @@ import {TabsPage} from './pages/tabs/tabs';
 import {BlogDetailPage} from "./pages/blog-detail/blog-detail";
 import {RouteConfig} from "angular2/router";
 import {ProfilePage} from "./pages/profile/profile";
+import {AuthHttp, AuthConfig} from "./helper/angular2-jwt";
+import {Http} from "angular2/http";
+import {provide} from "angular2/core";
 
 const ROUTES = [
   {path: '/app/blog/:id', component: BlogDetailPage},
@@ -12,8 +15,17 @@ const ROUTES = [
 
 @App({
   template: '<ion-nav [root]="rootPage"></ion-nav>',
-  config: {} // http://ionicframework.com/docs/v2/api/config/Config/
+  config: {}, // http://ionicframework.com/docs/v2/api/config/Config/
+  providers: [
+    provide(AuthHttp, {
+      useFactory: (http) => {
+        return new AuthHttp(new AuthConfig, http);
+      },
+      deps: [Http]
+    })
+  ]
 })
+
 @RouteConfig(ROUTES)
 export class MyApp {
   rootPage:any = TabsPage;
