3a978228cfc892d4544672cd5abaed56867560ac
Fengda HUANG <h@phodal.com>
2016-05-12 21:59:38 +0800

    add creat blogpost support

--- app/pages/blog/create-modal/create.ts
+++ app/pages/blog/create-modal/create.ts
@@ -1,16 +1,36 @@
 import {Page, ViewController} from "ionic-angular/index";
+import {AuthHttp} from "../../../helper/angular2-jwt";
+import {Headers} from "angular2/http";
 
 @Page({
   templateUrl: 'build/pages/blog/create-modal/create.html'
 })
 export class CreateBlogModal {
   private viewCtrl;
+  private authHttp;
+  private contentHeader;
 
-  constructor(viewCtrl: ViewController) {
+  constructor(viewCtrl:ViewController, authHttp: AuthHttp) {
+    this.authHttp = authHttp;
     this.viewCtrl = viewCtrl;
   }
 
   close() {
     this.viewCtrl.dismiss();
+  }
+
+  create(value) {
+    this.contentHeader = new Headers({"Content-Type": "application/json"});
+    this.authHttp.post('http://127.0.0.1:8000/api/blogpost/', JSON.stringify(value), {headers: this.contentHeader})
+      .map(res => res.json())
+      .subscribe(
+        data => this.authSuccess(data),
+        err => console.log(err)
+      );
+  }
+
+  authSuccess(data) {
+    console.log(data);
+    this.close()
   }
 }
